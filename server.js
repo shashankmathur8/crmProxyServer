118a1c0ef22f7d6a3ff11cc862074a58d85b9e75
Shashank Mathur <smathur4@slb.com>
2024-05-15 22:45:40 +0530

    test deployment

--- server.js
+++ server.js
@@ -14,22 +14,39 @@ app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
     res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+    userTargetUrl = 'https://ap-south-1.aws.data.mongodb-api.com/app/data-ehiqk/endpoint/data/v1/action/find';
+    _apiKey = "iYAP9lLJI4nTfzugswcGJWjb8MzQbp954mFxSskYIuYSafyPcgPEqOM228iE20yJ";
+
+    _dataSource = "Bettrillion";
+    _database = "crm";
+    _collection = "users";
 
     if (req.method === 'OPTIONS') {
         // CORS Preflight
         res.send();
     } else {
-        var targetURL = req.header('Target-URL');
+        var targetURL = userTargetUrl;
         if (!targetURL) {
             res.send(500, { error: 'There is no Target-Endpoint header in the request' });
             return;
         }
-        request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
+        request({
+            url: targetURL, method: 'POST', json: {
+                "dataSource": _dataSource,
+                "database": _database,
+                "collection": _collection,
+                "filter": { "username": "smathur4@slb.com", "password": "12345" },
+            }, headers: {
+                'apiKey': _apiKey,
+                "content-type": "application/json",
+                "Access-Control-Request-Headers": "*",
+            }
+        },
             function (error, response, body) {
                 if (error) {
                     console.error('error: ' + response.statusCode)
                 }
-//                console.log(body);
+               console.log(body);
             }).pipe(res);
     }
 });
